8c599ad7a21b711e3a3ef2e72eef2f31b6470a8e
NELLAIAPPAN S <nellaiappan264426@gmail.com>
2025-09-13 00:12:08 +0530

    sitemap update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "http://localhost:3002/", // ⚠️ Replace with your domain
+  siteUrl: "https://smeutools.vercel.app/", // ⚠️ Replace with your domain
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: "daily",
